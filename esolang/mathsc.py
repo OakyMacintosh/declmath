709c5df006c200f95ad13b94cdb40c1feb524a6d
SufremOak <migsufrem@gmail.com>
2025-04-05 16:34:26 -0300

    add DeclMath grammar and main interpreter

---
+++ esolang/mathsc.py
@@ -0,0 +1,48 @@
+from lark import Lark, Transformer, v_args
+
+with open("grammar.lark") as f:
+    grammar = f.read()
+
+parser = Lark(grammar, parser="lalr")
+
+class DeclEnv:
+    def __init__(self):
+        self.vars =  {}
+        self.funcs = {}
+        self.objects = {}
+        self.units = {
+            "M": 1e6,
+            "T": 1e12,
+            "P": 1e15
+        }
+
+env = DeclEnv()
+
+@v_args(inline=True)
+class Eval(Transformer):
+    def number(self, n): return float(n)
+    def var(self, name): return env.vers.get(name.value, 0)
+
+    def assign(self, name, value):
+        env.vars[name.value] = value
+    
+    def declare_obj(self, name, obj):
+        env.objects[name.value] = obj
+    
+    def obj(self, *items):
+        return [s.strip("") for s in items]
+
+    def func_def(self, name, expr):
+        env.funcs[name.value] = expr
+
+    def call(self, name, *args):
+        if name.value in env.funcs:
+            varname = "x"
+            old = env.vars.get(varname)
+            env.vars[varname] = args[0]
+            result = self.transform(env.funcs[name.value])
+            env.vars[varname] = old
+            return result
+        return 0
+
+    def expr_expr(): pass
